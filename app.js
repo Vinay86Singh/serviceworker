33d293d1e338bd313b2e6ea90f663a8416271e6a
Vinay86Singh <vinay86singh@gmail.com>
2015-12-14 18:17:08 +0530

    chnages

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
-// register service worker
-
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
-    
-    if(reg.installing) {
-      console.log('Service worker installing');
-    } else if(reg.waiting) {
-      console.log('Service worker installed');
-    } else if(reg.active) {
-      console.log('Service worker active');
-    }
-    
+  navigator.serviceWorker.register('/serviceworkertest/sw.js', { scope: '/serviceworkertest/' }).then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch(function(error) {
     // registration failed
     console.log('Registration failed with ' + error);
   });
 };
 
-// function for loading each image via XHR
 
-function imgLoad(imgJSON) {
-  // return a promise for an image loading
+getUpComingMovies = function(url){
+   
   return new Promise(function(resolve, reject) {
     var request = new XMLHttpRequest();
-    request.open('GET', imgJSON.url);
-    request.responseType = 'blob';
+    request.open('GET', url);
+    
 
     request.onload = function() {
       if (request.status == 200) {
         var arrayResponse = [];
-        arrayResponse[0] = request.response;
-        arrayResponse[1] = imgJSON;
-        resolve(arrayResponse);
+        var result = JSON.parse(request.response);
+        var movieData = result.result.data;
+        resolve(movieData);
       } else {
-        reject(Error('Image didn\'t load successfully; error code:' + request.statusText));
+        reject(Error(' error code:' + request.statusText));
       }
     };
 
@@ -44,32 +34,51 @@ function imgLoad(imgJSON) {
     // Send the request
     request.send();
   });
-};
+}
 
-var imgSection = document.querySelector('section');
-
-window.onload = function() {
-
-  // load each set of image, alt text, name and caption
-  for(i = 0; i<=Gallery.images.length-1; i++) {
-    imgLoad(Gallery.images[i]).then(function(arrayResponse) {
-
-      var myImage = document.createElement('img');
-      var myFigure = document.createElement('figure');
-      var myCaption = document.createElement('caption');
-      var imageURL = window.URL.createObjectURL(arrayResponse[0]);
-
-      myImage.src = imageURL;
-      myImage.setAttribute('alt', arrayResponse[1].alt);
-      myCaption.innerHTML = '<strong>' + arrayResponse[1].name + '</strong>: Taken by ' + arrayResponse[1].credit;
-
-      imgSection.appendChild(myFigure);
-      myFigure.appendChild(myImage);
-      myFigure.appendChild(myCaption);
-
-    }, function(Error) {
-      console.log(Error);
+window.onload = function(){
+    
+    var url ='http://staging.matrix.popkorn.in/v1/movielanding?tab=1&city_id=1&city_name=Delhi+NCR&lat=28.5664852&lng=77.31830479999999&page=1&limit=18&source=web&device=Mozilla&deviceId=b37817ce&rid=b37817ce&city_id=1&city_name=Delhi';
+    getUpComingMovies(url).then(function(res){
+        console.log(res.length);
+        console.log(res);
+        if(res !=='undefined')
+        {
+            var div = document.getElementById('movielist');
+            
+            res.forEach(function(ele, index){
+                var mainDiv = document.createElement('div');
+                mainDiv.style.float = 'left';
+                mainDiv.style.paddingRight = '15px';
+                mainDiv.style.paddingLight = '15px';
+                mainDiv.style.outline = "medium none";
+                var childDiv = document.createElement('div');
+                var imgEelement = document.createElement('img');
+                imgEelement.src = (ele.media.portraits[0].url!='' && typeof ele.media.portraits[0].url)?ele.media.portraits[0].url:ele.media.images[0].url;
+                var childDiv2 = document.createElement('div');
+                childDiv2.style.float = "left";
+                //childDiv2.style.float= 'left';
+                childDiv2.appendChild(imgEelement);
+                var childDiv3 = document.createElement('div');
+                childDiv3.style.float="left";
+                var textdiv = document.createElement('div');
+                textdiv.appendChild(document.createTextNode(ele.movie_name));
+                childDiv3.appendChild(textdiv);
+                
+                mainDiv.appendChild(childDiv2);
+                mainDiv.appendChild(childDiv3);
+                //div.appendChild(childDiv2);
+                //div.appendChild(childDiv3);
+                div.appendChild(mainDiv);
+ 
+                
+                
+                
+            });
+        }
+        
     });
-  };
+    
+}
 
-};
+
